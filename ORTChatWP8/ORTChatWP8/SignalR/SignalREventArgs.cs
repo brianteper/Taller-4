8b770960fe0a1b90c7194df9a5948f6588c494e7
Brian Teper <brian.teper@outlook.com>
2013-05-14 00:55:13 -0300

    versión que conecta WP8 a server

---
+++ ORTChatWP8/ORTChatWP8/SignalR/SignalREventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORTChatWP8.SignalR
+{
+    public class SignalREventArgs : EventArgs
+    {
+        public string ChatMessageFromServer
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ORTChatWP8/ORTChatWP8/SignalR/SignalRObjects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORTChatWP8.SignalR
+{
+    public class ChatClient
+    {
+        public string PhoneClientId { get; set; }
+        public string ChatUserName { get; set; }
+        public string ChatMessage { get; set; }
+    }
+}
